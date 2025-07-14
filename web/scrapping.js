4b74e6b11a14708d6575f28af2633e667769a326
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-14 17:38:46 -0400

    feat: enhance web scraping to handle pagination and retrieve product links

--- web/scrapping.js
+++ web/scrapping.js
@@ -2,47 +2,65 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const { pool } = require("../db/config");
+const baseUrl = "https://alogar.cl/collections/all";
 
 async function webScrapping() {
   try {
-    const { data } = await axios.get("https://alogar.cl/collections/all");
+    const { data } = await axios.get(baseUrl);
     const $ = cheerio.load(data);
 
-    const products = $(".grid-view-item.product-card")
-      .map((_, product) => {
-        const name = capitalize(
-          $(product).find(".product-card__title").text().trim()
-        );
-        const price = Number(
-          $(product)
-            .find(".price-item.price-item--regular")
-            .text()
-            .trim()
-            .replace("$", "")
-            .replace(".", "")
-        );
-        const image = `https:${$(product)
-          .find("img")[0]
-          .attribs["data-src"].replace("{width}", "180")}`;
-        const link = `https://alogar.cl${
-          $(product).find(".grid-view-item__link")[0].attribs["href"]
-        }`;
+    const pagination = $(".pagination__text").text().trim();
 
-        return { name, price, image, link };
-      })
-      .toArray();
+    const actualPage = pagination.split(" ")[1];
+    const finalPage = pagination.split(" ")[3];
 
-    if (products.length > 0) {
-      // Preparar parámetros dinámicamente
+    let finalProducts = [];
+
+    for (let i = actualPage; i <= finalPage; i++) {
+      const { data: pageData } = await axios.get(`${baseUrl}?page=${i}`);
+      const $ = cheerio.load(pageData);
+
+      const products = $(".grid-view-item.product-card")
+        .map((_, product) => {
+          const name = capitalize(
+            $(product).find(".product-card__title").text().trim()
+          );
+          const price = Number(
+            $(product)
+              .find(".price-item.price-item--regular")
+              .text()
+              .trim()
+              .replace("$", "")
+              .replace(".", "")
+          );
+          const image = `https:${$(product)
+            .find("img")[0]
+            .attribs["data-src"].replace("{width}", "180")}`;
+          const link = `https://alogar.cl${
+            $(product).find(".grid-view-item__link")[0].attribs["href"]
+          }`;
+          return { name, price, image, link };
+        })
+        .toArray();
+
+      finalProducts.push(products);
+    }
+
+    finalProducts = finalProducts.flat();
+
+    const selectQuery = "SELECT * FROM products";
+    const productsInDB = await pool.query(selectQuery);
+
+    if (productsInDB.rows.length === 0) {
       const values = [];
-      const placeholders = products
+      const placeholders = finalProducts
         .map((_, i) => {
           const offset = i * 4;
           values.push(
-            products[i].name,
-            products[i].price,
-            products[i].image,
-            products[i].link
+            finalProducts[i].name,
+            finalProducts[i].price,
+            finalProducts[i].image,
+            finalProducts[i].link
           );
           return `($${offset + 1}, $${offset + 2}, $${offset + 3}, $${
             offset + 4
@@ -57,10 +75,8 @@ async function webScrapping() {
 
       await pool.query(insertQuery, values);
       console.log(
-        `Se insertaron ${products.length} productos (o se ignoraron si ya existían).`
+        `Se insertaron ${finalProducts.length} productos (o se ignoraron si ya existían).`
       );
-    } else {
-      console.log("No se encontraron productos.");
     }
   } catch (error) {
     console.error("Error en webScrapping:", error);
