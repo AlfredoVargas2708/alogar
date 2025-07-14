629cf6e5ee31764a9d6576a3cfbd1f496022ed47
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-14 18:54:41 -0400

    feat: implement getProducts method in ProductsController for product retrieval with pagination

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,3 +1,21 @@
-class ProductsController {}
+const { pool } = require("../db/config");
 
-module.exports = ProductsController
+class ProductsController {
+  async getProducts(req, res) {
+    try {
+      const { page, pageSize } = req.query;
+      const limit = parseInt(pageSize, 10) || 10;
+      const offset = (Math.max(1, parseInt(page, 10) || 1) - 1) * limit;
+
+      const query = "SELECT * FROM products LIMIT $1 OFFSET $2";
+      const result = await pool.query(query, [limit, offset]);
+
+      res.status(200).json({ data: result.rows });
+    } catch (error) {
+      console.error("Error en getProducts:", error);
+      res.status(500).send({ message: "Internal Servel Error" });
+    }
+  }
+}
+
+module.exports = ProductsController;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,4 +3,6 @@ const router = require('express').Router();
 const { ProductsController } = require('../controllers/index')
 let ProdCtrl = new ProductsController
 
+router.get('/', ProdCtrl.getProducts);
+
 module.exports = router;
