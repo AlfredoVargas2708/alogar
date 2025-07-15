24e182d4740ee34fcbfbb277c768d5cb70c672a7
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-15 18:58:31 -0400

    feat: integrate Bootstrap for styling and enhance login component with toggle functionality

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-
+  // * Variables
+  isLogin: boolean = true;
 }
