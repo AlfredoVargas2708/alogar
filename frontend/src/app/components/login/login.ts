4a2965134cd9ed1303dc7809d882aa1d9f82c14e
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-15 19:54:15 -0400

    feat: enhance login component with Angular Material, form validation, and reset password modal

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { ResetPasswordModal } from './reset-password-modal/reset-password-modal';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -11,4 +13,37 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class Login {
   // * Variables
   isLogin: boolean = true;
+  loginForm!: FormGroup;
+  signUpForm!: FormGroup;
+
+  // * Constructor
+  constructor(private fb: FormBuilder, private modalService: MatDialog) {
+    this.loginForm = this.fb.group({
+      name: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    }, {
+      validators: this.passwordsMatchValidator
+    });
+  }
+
+  // * Validador de contraseñas iguales
+  passwordsMatchValidator(formGroup: FormGroup): ValidationErrors | null {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+
+  openRestartModal() {
+    this.modalService.open(ResetPasswordModal, {
+      width: '400px',
+      height: '250px'
+    });
+  }
+
 }
---
+++ frontend/src/app/components/login/reset-password-modal/reset-password-modal.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-reset-password-modal',
+  imports: [CommonModule, MatDialogModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './reset-password-modal.html',
+  styleUrl: './reset-password-modal.scss'
+})
+export class ResetPasswordModal {
+  // * Variables
+  resetPasswordForm!: FormGroup;
+
+  // * Constructor
+  constructor(private fb: FormBuilder) {
+    this.resetPasswordForm = this.fb.group({
+      name: ['', Validators.required]
+    })
+  }
+}
