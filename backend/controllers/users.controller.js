080593d79f1f1415b9e4923b6216c7e07ef5aa2e
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-18 19:48:27 -0400

    feat: add user sign-up functionality with email existence check

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,6 +1,29 @@
 const { pool } = require('../db/config');
 
 class UsersController {
+    async signUp(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            const query = `SELECT * FROM users WHERE email = ${email}`;
+
+            const result = await pool.query(query);
+
+            if (result.rows.length > 0) {
+                return res.status(400).send({ message: "El usuario ya existe" });
+            }
+
+            const insertQuery =  `INSERT INTO users (email, password) VALUES (${email}, ${password})`
+
+            await pool.query(insertQuery);
+
+            return res.status(201).send({ message: "Usuario regitrado correctamente" });
+        } catch (error) {
+            console.error('Error in signUp:', error);
+            res.status(500).send({ message: "Internal Server Error" });
+        }
+    }
+
     async resetPassword(req, res) {
         try {
             const { email, newPassword } = req.body;
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { UsersController } = require('../controllers/index');
 let UserCtrl = new UsersController();
 
+router.post('/sign-up', UserCtrl.signUp);
 router.post('/reset-password', UserCtrl.resetPassword);
 
 module.exports = router;
