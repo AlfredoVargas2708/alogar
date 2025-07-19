d081ae744870319188e1392448ea5a6e069fdceb
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-18 20:05:10 -0400

    feat: implement login functionality with error handling

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -4,6 +4,7 @@ const { UsersController } = require('../controllers/index');
 let UserCtrl = new UsersController();
 
 router.post('/sign-up', UserCtrl.signUp);
+router.post('/login', UserCtrl.login);
 router.post('/reset-password', UserCtrl.resetPassword);
 
 module.exports = router;
