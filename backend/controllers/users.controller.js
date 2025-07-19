e3460540a3ccbb3eb77442da31837a6c510f160a
AlfredoVargasG <alfvargas@alumnos.uai.cl>
2025-07-18 20:02:47 -0400

    refactor: improve signUp and resetPassword methods for better query handling

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,51 +1,53 @@
-const { pool } = require('../db/config');
+const { pool } = require("../db/config");
 
 class UsersController {
-    async signUp(req, res) {
-        try {
-            const { email, password } = req.body;
+  async signUp(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            const query = `SELECT * FROM users WHERE email = ${email}`;
+      const checkQuery = "SELECT * FROM users WHERE email = $1";
+      const checkResult = await pool.query(checkQuery, [email]);
 
-            const result = await pool.query(query);
+      if (checkResult.rows.length > 0) {
+        return res.status(400).send({ message: "El usuario ya existe" });
+      }
 
-            if (result.rows.length > 0) {
-                return res.status(400).send({ message: "El usuario ya existe" });
-            }
+      const insertQuery = "INSERT INTO users (email, password) VALUES ($1, $2)";
+      await pool.query(insertQuery, [email, password]);
 
-            const insertQuery =  `INSERT INTO users (email, password) VALUES (${email}, ${password})`
-
-            await pool.query(insertQuery);
-
-            return res.status(201).send({ message: "Usuario regitrado correctamente" });
-        } catch (error) {
-            console.error('Error in signUp:', error);
-            res.status(500).send({ message: "Internal Server Error" });
-        }
+      return res
+        .status(201)
+        .send({ message: "Usuario registrado correctamente" });
+    } catch (error) {
+      console.error("Error in signUp:", error);
+      res.status(500).send({ message: "Internal Server Error" });
     }
+  }
 
-    async resetPassword(req, res) {
-        try {
-            const { email, newPassword } = req.body;
+  async resetPassword(req, res) {
+    try {
+      const { email, newPassword } = req.body;
 
-            const query = `SELECT * FROM users WHERE email = ${email}`;
+      const query = `SELECT * FROM users WHERE email = $1`;
 
-            const result = await pool.query(query);
+      const result = await pool.query(query, [email]);
 
-            if (result.rows.length === 0) {
-                return res.status(404).send({ message: "Usuario no encontrado"});
-            }
+      if (result.rows.length === 0) {
+        return res.status(404).send({ message: "Usuario no encontrado" });
+      }
 
-            const updateQuery = `UPDATE users SET password = ${newPassword} WHERE email = ${email}`
+      const updateQuery = `UPDATE users SET password = $1 WHERE email = $2`;
 
-            await pool.query(updateQuery);
+      await pool.query(updateQuery, [newPassword, email]);
 
-            return res.status(200).send(({ message: "Contraseña actualizada correctamente" }));
-        } catch (error) {
-            console.error('Error in resetPassword:', error);
-            res.status(500).send({ message: "Internal Server Error" });
-        }
+      return res
+        .status(200)
+        .send({ message: "Contraseña actualizada correctamente" });
+    } catch (error) {
+      console.error("Error in resetPassword:", error);
+      res.status(500).send({ message: "Internal Server Error" });
     }
+  }
 }
 
 module.exports = UsersController;
